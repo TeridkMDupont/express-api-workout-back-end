988e87cc0b219912d6db79eb02bf1ce85153e815
TerickD <TerickMDupont@gmail.com>
2025-11-05 17:33:56 -0500

    delete workout route complete

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -59,6 +59,20 @@ router.put('/:workoutId', async (req, res) => {
     }
 });
 
+//DELETE /workouts/:workoutId - Delete
+router.delete('/:workoutId', async (req, res) => {
+    try {
+        const workout = await Workout.findById(req.params.workoutId);
+        if(!workout.author.equals(req.user._id)) {
+            return res.status(403).send("You can only delete your own Workouts!");
+        }
+        const deletedWorkout = await Workout.findByIdAndDelete(req.params.workoutId)
+        res.status(200).json(deletedWorkout);
+    } catch (err) {
+        res.status(500).json({err: err.message});
+    }
+});
+
 
 
 module.exports = router;
