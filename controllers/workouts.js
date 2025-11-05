661d25df8add4773eefc3a5e7a422a3ba49e64f2
TerickD <TerickMDupont@gmail.com>
2025-11-04 20:14:44 -0500

    create a work out back end complete

---
+++ controllers/workouts.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const verifyToken = require('../middleware/verify-token');
+const Workout = require('../models/workout')
+const router = express.Router();
+
+router.use(verifyToken);
+
+
+//POST /workouts
+router.post('/', async (req, res) => {
+    try {
+        req.body.author = req.user._id;
+        const workout = await Workout.create(req.body);
+        workout._doc.author = req.user;
+        res.status(201).json(workout);
+    }catch (err) {
+        res.status(500).json({err: err.message});
+    }
+})
+
+
+
+
+module.exports = router;
