87acda27fff57fcf331d30773f969b329f120da0
TerickD <TerickMDupont@gmail.com>
2025-11-07 18:08:01 -0500

    update and delete routes complete

--- controllers/exercise.js
+++ controllers/exercise.js
@@ -30,7 +30,7 @@ router.get('/exercises', async (req, res) => {
     }
 });
 
-//GET /exercises/:exerciseId - Show
+//GET /exercises/:exerciseId - Get one Exercise
 router.get('/:workoutId', async (req, res) => {
     try { 
         const exercise = await Exercise.findById(req.params.workoutId).populate([
@@ -39,6 +39,40 @@ router.get('/:workoutId', async (req, res) => {
         res.status(200).json(exercise)
     }catch (err) {
         res.status(500).json({err: err.message})
+    }
+});
+
+
+//PUT /exercises/:exerciseId -Update an Exercise
+router.put('/:exerciseId', async (req, res) => {
+    try {
+        const exercise = await Exercise.findById(req.params.exerciseId);
+        if (!exercise.author.equals(req.user._id)) {
+            return res.status(403).send("You can only update your own Exercises!")
+        };
+        const updatedExercise = await Exercise.findByIdAndUpdate(
+            req.params.exerciseId,
+            req.body,
+            { new: true }
+        );
+        updatedExercise._doc.author = req.user;
+        res.status(200).json(updatedExercise);
+    }catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+//DELETE /exercises/:exerciseId - Delete an Exercise
+router.delete('/:exerciseId', async (req, res) => {
+    try {
+        const exercise = await Exercise.findById(req.params.exerciseId);
+        if(!exercise.author.equals(req.user._id)) {
+            return res.status(403).send("You can only delete your own Exercises!");
+        }
+        const deletedExercise = await Exercise.findByIdAndDelete(req.params.exerciseId)
+        res.status(200).json(deletedExercise);
+    } catch (err) {
+        res.status(500).json({err: err.message});
     }
 });
 
